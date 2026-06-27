615e6d69120c3943cdcfc7ad150283e9dad0cee4
Raphaelo48 <97660857+Raphaelo48@users.noreply.github.com>
2026-06-27 14:01:59 +0300

    Update storage.js

--- frontend/js/storage.js
+++ frontend/js/storage.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 const DB_KEY = "nutrition_results";
-const API_URL = "https://web-opros-production.up.railway.app/"
+const API_URL = "https://web-opros-production.up.railway.app/api"
 const Storage = {
     // --- БАЗОВЫЕ ОПЕРАЦИИ ---
     
